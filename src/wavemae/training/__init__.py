e22edb68458e201b82a2283e6097f3347e43ea73
Mantis-Ryuji <ryuji0801yama@icloud.com>
2025-09-24 17:38:17 +0900

    v0.1.0

--- src/wavemae/training/__init__.py
+++ src/wavemae/training/__init__.py
@@ -1,7 +1,6 @@
 from .trainer import Trainer, TrainerConfig
 from .tester import Tester
 from .extracter import Extracter, ExtractConfig
-from .samplers import compute_reference_vector, make_weighted_sampler_by_cosine
 from .optim import build_optimizer, build_scheduler
 
 __all__ = [
@@ -10,8 +9,6 @@ __all__ = [
     "Tester",
     "Extracter", 
     "ExtractConfig",
-    "compute_reference_vector",
-    "make_weighted_sampler_by_cosine",
     "build_optimizer",
     "build_scheduler"
 ]
